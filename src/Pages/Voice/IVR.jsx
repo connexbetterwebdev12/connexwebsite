5f1615b0c5aa65f656a40cf6dadf6e60f6412d54
Yashwardhan singh thakur <yashwardhansinghthakur881@gmail.com>
2025-03-02 12:41:36 +0530

    fixed file name related issue

--- src/Pages/Voice/IVR.jsx
+++ src/Pages/Voice/IVR.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import ContentImageBlock from '../../CommonComponent/ContentImageBlock';
-import Boxcontainer from '../../CommonComponent/Boxconatainer';
-import { Accordian } from '../../CommonComponent/Accordian';
-import { Button } from '../../CommonComponent/Button';
-import LeftRightBLock from '../../CommonComponent/LeftRightBLock';
-import CommonHeading from '../../CommonComponent/CommonHeading';
-import Scrollcomponent from '../../CommonComponent/Scrollcomponent';
-import FAQ from '../../CommonComponent/FAQ';
-import ivrbigimage from "../../assets/VoiceImage/IMAGE/asdcqdfcaeqsdqa.webp"
-import {ivrBox,ivrziczac,voiceBox,IVRFAQ,IVRDLT,IVRcroll} from '../../Constant/Voice';
-import voice_hero from "../../assets/VoiceImage/lottie/Services-Voice-Hero (1).json";
-import Agirlwithtext from "../../CommonComponent/Agirlwithtext"
+import React from "react";
+import ContentImageBlock from "../../CommonComponent/ContentImageBlock";
+import Boxcontainer from "../../CommonComponent/Boxconatainer";
+import { Accordian } from "../../CommonComponent/Accordian";
+import { Button } from "../../CommonComponent/Button";
+import LeftRightBLock from "../../CommonComponent/LeftRightBLock";
+import CommonHeading from "../../CommonComponent/CommonHeading";
+import Scrollcomponent from "../../CommonComponent/Scrollcomponent";
+import FAQ from "../../CommonComponent/FAQ";
+import ivrbigimage from "../../assets/VoiceImage/IMAGE/asdcqdfcaeqsdqa.webp";
+import {
+  ivrBox,
+  ivrziczac,
+  voiceBox,
+  IVRFAQ,
+  IVRDLT,
+  IVRcroll,
+} from "../../Constant/Voice";
+import voice_hero from "../../assets/VoiceImage/lottie/Services-Voice-Hero.json";
+import Agirlwithtext from "../../CommonComponent/Agirlwithtext";
 
 const sections = [
   {
@@ -18,8 +25,10 @@ const sections = [
     props: {
       subheading: "Best IVR Service Provider In India",
       heading: "Interactive Voice Response System (IVR) for your Business",
-      content: "Discover the best Interactive Voice Response (IVR) system in India, designed to streamline customer interactions and improve efficiency for businesses of all sizes with our tailored IVR solutions.",
-      secheading:"Integrated Voice Response | IVR Software | Cloud IVR | Hosted IVR",
+      content:
+        "Discover the best Interactive Voice Response (IVR) system in India, designed to streamline customer interactions and improve efficiency for businesses of all sizes with our tailored IVR solutions.",
+      secheading:
+        "Integrated Voice Response | IVR Software | Cloud IVR | Hosted IVR",
       lottie: "use",
       img: voice_hero,
     },
@@ -37,15 +46,13 @@ const sections = [
     props: {
       h: "Features of IVR System for Enhanced Customer Engagement",
       p: "Our IVR as a Service offers businesses a comprehensive solution to automate customer interactions and improve service delivery. With our advanced IVR call centre solutions.",
-     
     },
   },
 
   {
     component: LeftRightBLock,
-     props: { data: ivrziczac },
+    props: { data: ivrziczac },
   },
-  
 
   {
     component: Boxcontainer,
@@ -59,65 +66,55 @@ const sections = [
   {
     custom: () => (
       <div className="container grid md:grid-cols-2 items-center">
-        <CommonHeading
-          h="Advanced Features of Our IVR Solutions"
-          p=""
-        />
-        
+        <CommonHeading h="Advanced Features of Our IVR Solutions" p="" />
+
         <Scrollcomponent data={IVRcroll} />
       </div>
     ),
   },
-    {
-      custom: () => (
-        <div className="container grid md:grid-cols-2 items-center gap-8">
+  {
+    custom: () => (
+      <div className="container grid md:grid-cols-2 items-center gap-8">
         <div>
-          {
-            IVRDLT.map((ele,ind)=>
-              <Accordian val={ele} key={ind}></Accordian>
-  
-            )
-          }
+          {IVRDLT.map((ele, ind) => (
+            <Accordian val={ele} key={ind}></Accordian>
+          ))}
         </div>
-        <div className=''>
-        <div className="bg-blue-50  p-6 rounded-lg shadow-md flex flex-col gap-4  ">
-        <div className="border-b-2 border-blue-400 pb-2">
-          <h1 className="text-blue-600 text-2xl font-bold">Connex Better Smart IVR Solution for Enterprises</h1>
+        <div className="">
+          <div className="bg-blue-50  p-6 rounded-lg shadow-md flex flex-col gap-4  ">
+            <div className="border-b-2 border-blue-400 pb-2">
+              <h1 className="text-blue-600 text-2xl font-bold">
+                Connex Better Smart IVR Solution for Enterprises
+              </h1>
+            </div>
+            <p className="text-gray-700 mt-4">
+              Do you have any additional questions you would like to ask?
+            </p>
+            <Button name="Contact Now" link="/Contact"></Button>
+          </div>
         </div>
-        <p className="text-gray-700 mt-4">
-          Do you have any additional questions you would like to ask?
-        </p>
-        <Button name="Contact Now" link="/Contact"></Button>
       </div>
+    ),
+  },
+  {
+    custom: () => (
+      <div className="container">
+        <CommonHeading h="What are the significant use cases of IVR?" p="" />
+        <img
+          src={ivrbigimage}
+          alt="image of Authentication"
+          loading="lozy"
+        ></img>
       </div>
-       
-          
-        </div>
-      ),
-    },
-     {
-            custom: () => (
-              <div className="container">
-                       <CommonHeading
-                  h="What are the significant use cases of IVR?"
-                  p=""
-                />
-              <img src={ ivrbigimage } alt="image of Authentication" loading='lozy'></img>
-       
-            
-              </div>
-            ),
-          },
-{
-          component:FAQ,
-          props:{data:IVRFAQ}
-         },
-         {
-          component: Agirlwithtext,
-          
-        },
-  
-
+    ),
+  },
+  {
+    component: FAQ,
+    props: { data: IVRFAQ },
+  },
+  {
+    component: Agirlwithtext,
+  },
 ];
 
 const IVR = () => {
@@ -137,4 +134,3 @@ const IVR = () => {
 };
 
 export default IVR;
-
